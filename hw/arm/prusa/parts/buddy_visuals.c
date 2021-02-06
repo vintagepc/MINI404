a458188fcfc1e7eba635c11595f480ac7a2ad614
VintagePC <53943260+vintagepc@users.noreply.github.com>
2021-02-05 21:35:53 -0500

    convert to SHM IPC (closes #27)

--- hw/arm/prusa/parts/buddy_visuals.c
+++ hw/arm/prusa/parts/buddy_visuals.c
@@ -16,9 +16,18 @@
 #include "hw/sysbus.h"
 #include "migration/vmstate.h"
 #include "qemu/module.h"
-#include "ui/console.h"
+#include "qemu/timer.h"
+// #include "ui/console.h"
 #include "qom/object.h"
 
+#define MQ 0
+#define FILE 0
+#define SHM 1
+#if MQ
+#include <mqueue.h>
+#elif SHM
+#include "../3rdParty/shmemq-blog/shmemq.h"
+#endif
 //#define DEBUG_buddy_visuals 1
 
 #ifdef DEBUG_buddy_visuals
@@ -37,15 +46,25 @@ do { fprintf(stderr, "buddy_visuals: error: " fmt , ## __VA_ARGS__);} while (0)
 #define DPY_ROWS 320
 #define DPY_COLS 500
 
+// 20 kB
+
 struct buddy_visuals_state {
     SysBusDevice parent;
-    QemuConsole *con;
-
+    // QemuConsole *con;
+#if MQ
+    mqd_t queue;
+    char mq_buffer[8];
+#elif FILE
+#define BUFFER_SIZE 1024U*20U 
     FILE *fd_pipe;
-
+    char buffer[BUFFER_SIZE];
+#else
+    shmemq_t *queue;
+#endif
     bool is_opened;
 
     int32_t redraw;
+    QEMUTimer *timer_flush;
 
 };
 
@@ -142,19 +161,76 @@ OBJECT_DECLARE_SIMPLE_TYPE(buddy_visuals_state, BUDDY_VISUALS)
 
 static void buddy_visuals_add_motor(buddy_visuals_state *s, unsigned char index, unsigned char label, uint32_t steps_per_mm, int32_t max_steps, bool is_simple)
 {
-    fprintf(s->fd_pipe,"%cAM",2);
+#if MQ    
+    if (mq_send(s->queue, "AM", 3,0)) {
+#elif SHM
+    char addmsg[] = {'A','M', 0,0,0,0,0};
+    if (!shmemq_try_enqueue(s->queue, addmsg, sizeof(addmsg))) { 
+#endif           
+        fprintf(stderr,"Failed to send IPC message!\n");
+        perror("AM");
+    }
+  
+    // fprintf(s->fd_pipe,"%cAM",2);
 
-    fprintf(s->fd_pipe,"%cM%cL%c",4,index+'0', label);
-    fprintf(s->fd_pipe,"%cM%cS%c",4,index+'0', is_simple+'0');
+#if MQ    
+    char msg[] = {'M','0' + index, 'L', label };
+    if (mq_send(s->queue, msg, sizeof(msg),0)) {
+#elif SHM
+    char msg[] = {'M','0' + index, 'L', label,0,0,0 };
+    if (!shmemq_try_enqueue(s->queue, msg, sizeof(msg))) { 
+#endif           
+        fprintf(stderr,"Failed to send IPC message!\n");
+    }
+  
+    msg[2] = 'S';
+    msg[3] = '0' + is_simple;
+#if MQ
+    if (mq_send(s->queue, msg, sizeof(msg),0)) {
+#elif SHM
+    if (!shmemq_try_enqueue(s->queue, msg, sizeof(msg))) { 
+#endif           
+        fprintf(stderr,"Failed to send IPC message!\n");
+    }
+
+    // fprintf(s->fd_pipe,"%cM%cL%c",4,index+'0', label);
+    // fprintf(s->fd_pipe,"%cM%cS%c",4,index+'0', is_simple+'0');
     
-    fprintf(s->fd_pipe,"%cM%cU",7,index+'0');
-    for (int i=3; i>=0; i--)
-        fputc(steps_per_mm>>(8*i),s->fd_pipe);
+    char msg2[] = {'M','0' + index , 'U', (steps_per_mm>>24)&0xFF, (steps_per_mm>>16)&0xFF, (steps_per_mm>>8)&0xFF, (steps_per_mm)&0xFF};
+#if MQ    
+    if (mq_send(s->queue, msg2, sizeof(msg2),0)) {
+#elif SHM
+    if (!shmemq_try_enqueue(s->queue, msg2, sizeof(msg2))) { 
+#endif           
+        fprintf(stderr,"Failed to send IPC message!\n");
+    }
 
-    fprintf(s->fd_pipe,"%cM%cX",7,index+'0');
-    for (int i=3; i>=0; i--)
-        fputc(max_steps>>(8*i),s->fd_pipe);
-    fflush(s->fd_pipe);
+    // fprintf(s->fd_pipe,"%cM%cU",7,index+'0');
+    // for (int i=3; i>=0; i--)
+    //     fputc(steps_per_mm>>(8*i),s->fd_pipe);
+    msg2[2] = 'X';
+    msg2[3] = (max_steps>>24)&0xFF;
+    msg2[4] = (max_steps>>16)&0xFF;
+    msg2[5] = (max_steps>>8)&0xFF;
+    msg2[6] = (max_steps)&0xFF;
+#if MQ    
+    if (mq_send(s->queue, msg2, sizeof(msg2),0)) {
+#elif SHM
+    if (!shmemq_try_enqueue(s->queue, msg2, sizeof(msg2))) { 
+#endif   
+        fprintf(stderr,"Failed to send IPC message!\n");
+    }
+    //fprintf(s->fd_pipe,"%cM%cX",7,index+'0');
+    // for (int i=3; i>=0; i--)
+    //     msg[3+(max_steps>>(8*i),s->fd_pipe);
+    // fflush(s->fd_pipe);
+}
+inline static void buddy_visuals_schedule_flush(buddy_visuals_state *s)
+{
+    // if (!timer_pending(s->timer_flush))
+    // {
+    //     timer_mod(s->timer_flush, qemu_clock_get_ms(QEMU_CLOCK_VIRTUAL)+50);
+    // }
 }
 
 static void buddy_visuals_set_indicator_logic(void *opaque, int n, int value)
@@ -162,8 +238,18 @@ static void buddy_visuals_set_indicator_logic(void *opaque, int n, int value)
     buddy_visuals_state *s = opaque;
     if (!s->is_opened)
         return;
-    fprintf(s->fd_pipe, "%cI%cV%c",4, n+'0', (255*(value>0)));
-    fflush(s->fd_pipe);
+#if MQ
+    char msg[] = {'I','0'+n,'V', (255*(value>0)) };
+    if (mq_send(s->queue, msg, sizeof(msg),0)) {
+#elif SHM
+    char msg[] = {'I','0'+n,'V', (255*(value>0)),0,0,0 };
+    if (!shmemq_try_enqueue(s->queue, msg, sizeof(msg))) { 
+#endif   
+        fprintf(stderr,"Failed to send IPC message!\n");
+    }
+  
+    // fprintf(s->fd_pipe, "%cI%cV%c",4, n+'0', (255*(value>0)));
+    // buddy_visuals_schedule_flush(s);
 }
 
 static void buddy_visuals_set_indicator_analog(void *opaque, int n, int value)
@@ -171,21 +257,81 @@ static void buddy_visuals_set_indicator_analog(void *opaque, int n, int value)
     buddy_visuals_state *s = opaque;
     if (!s->is_opened)
         return;
-
-    fprintf(s->fd_pipe, "%cI%cV%c",4, n+'0', value&0xFF);
-    fflush(s->fd_pipe);
+  
+#if MQ    
+    char msg[] = {'I','0'+n,'V', value&0xFF };
+    if (mq_send(s->queue, msg, sizeof(msg),0)) {
+#elif SHM
+    char msg[] = {'I','0'+n,'V', value&0xFF,0,0,0 };
+    if (!shmemq_try_enqueue(s->queue, msg, sizeof(msg))) { 
+#endif        
+        fprintf(stderr,"Failed to send IPC message!\n");
+    }
+  
+    // fprintf(s->fd_pipe, "%cI%cV%c",4, n+'0', value&0xFF);
+    // buddy_visuals_schedule_flush(s);
 }
 
 
 static void buddy_visuals_add_indicator(buddy_visuals_state *s, unsigned char index, unsigned char label, uint32_t color)
 {
-    fprintf(s->fd_pipe,"%cAI%c",3,label);
+    // fprintf(s->fd_pipe,"%cAI%c",3,label);
+#if MQ
+    char msg[] = {'A','I', label };
+    if (mq_send(s->queue, msg, sizeof(msg),0)) {
+#elif SHM
+    char msg[] = {'A','I', label ,0,0,0,0};
+    if (!shmemq_try_enqueue(s->queue, msg, sizeof(msg))) { 
+#endif
+        fprintf(stderr,"Failed to send IPC message!\n");
+    }
 
-    fprintf(s->fd_pipe,"%cI%cC",7,index+'0');
-    for (int i=3; i>=0; i--)
-        fputc(color>>(8*i),s->fd_pipe);
+    char msg2[] = {'I','0' + index , 'C', (color>>24)&0xFF, (color>>16)&0xFF, (color>>8)&0xFF, (color)&0xFF};
+#if MQ
+    if (mq_send(s->queue, msg2, sizeof(msg2),0)) {
+#elif SHM
+    if (!shmemq_try_enqueue(s->queue, msg2, sizeof(msg2))) { 
+#endif        
+        fprintf(stderr,"Failed to send IPC message!\n");
+    }
+    // fprintf(s->fd_pipe,"%cI%cC",7,index+'0');
+    // for (int i=3; i>=0; i--)
+    //     fputc(color>>(8*i),s->fd_pipe);
 
     buddy_visuals_set_indicator_logic(s, index, 0);
+}
+
+// Used to flush the queue at regular intervals when stepping, as high step rates can slog things down.
+// static void
+// buddy_visuals_flush_timer(void *opaque)
+// {
+//     buddy_visuals_state *s = opaque;
+// //    fflush(s->fd_pipe);
+// }
+
+
+static void buddy_visuals_inverted_step_in(void *opaque, int n, int level)
+{
+    buddy_visuals_state *s = opaque;
+    if (!s->is_opened)
+        return;
+    int temp = -level;
+    char msg[] = {'M','0' + n, 'P', 0,0,0,0};
+    memcpy(msg+3, &temp, 4);
+#if MQ
+    if (mq_send(s->queue, msg, sizeof(msg),0)) {
+#elif SHM
+    if (!shmemq_try_enqueue(s->queue, msg, sizeof(msg))) { 
+#endif
+        fprintf(stderr,"Failed to send IPC message!\n");
+    }
+    // fprintf(s->fd_pipe, "%cM%cP",7, n+'0');
+    // // int32_t pos = level;
+    // for (int i=3; i>=0; i--)
+    //     fputc(level>>(8*i),s->fd_pipe);
+
+    // buddy_visuals_schedule_flush(s);
+  
 }
 
 static void buddy_visuals_step_in(void *opaque, int n, int level)
@@ -193,12 +339,23 @@ static void buddy_visuals_step_in(void *opaque, int n, int level)
     buddy_visuals_state *s = opaque;
     if (!s->is_opened)
         return;
+    
+    char msg[] = {'M','0' + n, 'P', 0,0,0,0};
+    memcpy(msg+3, &level, 4);
+#if MQ
+    if (mq_send(s->queue, msg, sizeof(msg),0)) {
+#elif SHM
+    if (!shmemq_try_enqueue(s->queue, msg, sizeof(msg))) { 
+#endif
+        fprintf(stderr,"Failed to send IPC message!\n");
+    }
+    // fprintf(s->fd_pipe, "%cM%cP",7, n+'0');
+    // // int32_t pos = level;
+    // for (int i=3; i>=0; i--)
+    //     fputc(level>>(8*i),s->fd_pipe);
 
-    fprintf(s->fd_pipe, "%cM%cP",7, n+'0');
-    // int32_t pos = level;
-    for (int i=3; i>=0; i--)
-        fputc(level>>(8*i),s->fd_pipe);
-    fflush(s->fd_pipe);
+    // buddy_visuals_schedule_flush(s);
+  
 }
 
 
@@ -207,22 +364,38 @@ static void buddy_visuals_enable_in(void *opaque, int n, int level)
     buddy_visuals_state *s = opaque;
     if (!s->is_opened)
         return;
-
-    fprintf(s->fd_pipe, "%cM%cE%c",4, n+'0','0' +(level>0));
-    fflush(s->fd_pipe);
+#if MQ
+    char msg[] = {'M','0' + n, 'E', '0'+ (level>0)};
+    if (mq_send(s->queue, msg, sizeof(msg),0)) {
+#elif SHM
+    char msg[] = {'M','0' + n, 'E', '0'+ (level>0),0,0,0};
+    if (!shmemq_try_enqueue(s->queue, msg, sizeof(msg))) { 
+#endif        
+        fprintf(stderr,"Failed to send IPC message!\n");
+    }
+//    fflush(s->fd_pipe);
 }
-
 
 static void buddy_visuals_realize(Object *obj)
 {
     DeviceState *dev = DEVICE(obj);
     buddy_visuals_state *s = BUDDY_VISUALS(obj);
     qdev_init_gpio_in_named(dev, buddy_visuals_step_in, "motor-step",4);
+    qdev_init_gpio_in_named(dev, buddy_visuals_inverted_step_in, "motor-invstep",4);
     qdev_init_gpio_in_named(dev, buddy_visuals_enable_in, "motor-enable",4);
     qdev_init_gpio_in_named(dev, buddy_visuals_set_indicator_analog, "indicator-analog",10);
     qdev_init_gpio_in_named(dev, buddy_visuals_set_indicator_logic, "indicator-logic",10);
+    const char IPC_FILE[] = "/MK404IPC";
+    
+#if MQ
+    s->queue = mq_open(IPC_FILE, O_WRONLY );
+    if (s->queue == (mqd_t)-1) {
+        printf("Could not open message queue. Skipping connection.\n");
+        return;
+    }
+    mq_send(s->queue, "C",1,0);
+#elif FILE
     struct stat file;
-    const char IPC_FILE[] = "MK404.IPC";
     if (stat(IPC_FILE, &file)<0)
     {
         printf("MK404.IPC file not found. Skipping connection.\n");
@@ -235,6 +408,16 @@ static void buddy_visuals_realize(Object *obj)
         printf("Could not open IPC file\n");
         return;
     }
+    memset(s->buffer, 0,BUFFER_SIZE);
+    setbuffer(s->fd_pipe, s->buffer,BUFFER_SIZE);
+    s->timer_flush = timer_new_ms(QEMU_CLOCK_VIRTUAL, buddy_visuals_flush_timer, s);
+#else
+    s->queue = shmemq_new(IPC_FILE, 100, 7);
+
+    char MSG[] = {'C', 0,0,0,0,0,0};
+    shmemq_try_enqueue(s->queue, MSG, sizeof(MSG));
+
+#endif
     s->is_opened = true;
     // Pipe opened, configure the motor....
 
@@ -254,6 +437,14 @@ static void buddy_visuals_realize(Object *obj)
     buddy_visuals_add_indicator(s, 7,'M', 0xFF000000); // Z-probe/minda
     buddy_visuals_add_indicator(s, 8,'H', 0xFF000000); // E heater
     buddy_visuals_add_indicator(s, 9,'B', 0xFF000000); // Bed heater
+
+#if MQ
+    struct mq_attr attr, attr2;
+    mq_getattr(s->queue, &attr);
+    mq_getattr(s->queue, &attr2);
+    attr.mq_flags = O_NONBLOCK; // Change to nonblock now that setup is done.
+    mq_setattr(s->queue, &attr, &attr2);
+#endif
 
 
 }
