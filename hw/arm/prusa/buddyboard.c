69a310da5b0f0be9626d963dd335cbea1cdc744b
vintagepc <53943260+vintagepc@users.noreply.github.com>
2021-02-15 15:34:55 -0500

    Fix bad initial temp from testing

--- hw/arm/prusa/buddyboard.c
+++ hw/arm/prusa/buddyboard.c
@@ -172,7 +172,7 @@ static void buddy_init(MachineState *machine)
         // qdev_connect_gpio_out_named(DEVICE(&SOC->usart2),"tmc2209_usart_cs",0, qdev_get_gpio_in_named(tmc, SSI_GPIO_CS, 0));
     }
 
-    uint16_t startvals[] = {-40,18, 25, 512, 512};
+    uint16_t startvals[] = {18,18, 25, 512, 512};
     uint8_t channels[] = {10,4,3,5,6};
     int tables[] = {5, 1, 2000,0,0};
     DeviceState *bed = NULL, *hotend = NULL;
