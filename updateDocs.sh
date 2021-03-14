8b14f5fd0f52ff23c0c72f82f50ad993c5babe92
vintagepc <53943260+vintagepc@users.noreply.github.com>
2021-03-14 16:01:45 -0400

    Fix machine name

--- updateDocs.sh
+++ updateDocs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 touch dummy.bin
-build/qemu-system-buddy -machine prusabuddy -kernel dummy.bin -append scripthelpmd | grep -A500 "# Scripting" > ref/autogen/Scripting-Mini.md
+build/qemu-system-buddy -machine prusa-mini -kernel dummy.bin -append scripthelpmd | grep -A500 "# Scripting" > ref/autogen/Scripting-Mini.md
 
